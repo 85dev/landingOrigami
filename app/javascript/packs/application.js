e92f73f38a22f09c3a453ffcd3716c74983ed909
85dev <mercier.ncls@gmail.com>
2022-03-22 11:52:32 +0100

    front stuff forms

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,10 +17,53 @@ import "@lottiefiles/lottie-player";
 
 
 document.addEventListener('turbolinks:load', () => {
+
+    function addEventListenerList(list, event, fn) {
+        for (var i = 0, len = list.length; i < len; i++) {
+            list[i].addEventListener(event, fn, false);
+        }
+    }
+
     const click = document.querySelector('.menu-path');
     const dropdown = document.querySelector('.responsive-menu');
 
     click.addEventListener('click', () => {
         dropdown.classList.toggle('newlist');
     })
+
+    const buttonNewsletter = document.querySelectorAll(".first");
+    const buttonBeta = document.querySelectorAll('.second');
+    const formBeta = document.querySelector('.form-group');
+    const formNewsletter = document.querySelector('.form-group-news');
+
+
+    addEventListenerList(buttonNewsletter, 'click', () => {
+        if (formNewsletter.style.display == 'none') {
+            formNewsletter.style.display = "flex";
+            buttonBeta.style.display = "none";
+        } else if (formBeta.style.display == 'flex' || formNewsletter.style.display == 'none') {
+            formNewsletter.style.display = "flex";
+            buttonBeta.style.display = "block";
+            formBeta.style.display == 'none';
+        } else {
+            formNewsletter.style.display = "none";
+            buttonBeta.style.display = "block";
+        }
+    })
+
+    addEventListenerList(buttonBeta, 'click', () => {
+        if (formBeta.style.display == 'none') {
+            formBeta.style.display = "flex";
+            buttonNewsletter.style.display = "none";
+            formNewsletter.style.display = "none";
+        } else if (formNewsletter.style.display == 'flex' || formNewsletter.style.display == 'none') {
+            formNewsletter.style.display = "none";
+            formBeta.style.display = "flex";
+            buttonNewsletter.style.display = "none";
+        } else {
+            formBeta.style.display = "none";
+            buttonNewsletter.style.display = "block";
+        }
+    })
 })
+
