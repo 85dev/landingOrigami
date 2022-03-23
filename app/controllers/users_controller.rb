0f14c871f3a556c46871bc3b716e4622d2df86ec
Clément Sapt <csapt2001@gmail.com>
2022-03-23 15:57:48 +0100

    add create.js.erb and fix form :construction:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
+
   def create
     @user = User.new(user_params)
-    UserMailer.with(user: @user).welcome_email.deliver_later if @user.save
-    redirect_to root_path
+    UserMailer.with(user: @user).welcome_email.deliver_later  if @user.save
+    respond_to do |format|
+      format.js
+    end
   end
 
   def create_helper
